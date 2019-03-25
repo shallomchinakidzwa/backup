3a70f56b1d14620a004d4e395ef2bdddb78107cd
SHALLOM <chinakidzwashallom@gmail.com>
2019-03-25 11:56:02 +0200

    loving it

---
+++ MEAN/index.js
@@ -0,0 +1,13 @@
+console.log("hi this is my first server");
+
+var one = 1
+var two = 2
+var three = one * two
+console.log(three);
+//
+var four = [1,2,3,4,5,6,7,8,9,10]
+
+for(i=0 ; i<four.length ; i++){console.log("the index is " + i + "the value is " + four[i]);
+}
+//
+
